486e70794f75f975016b97897f6c4a41a5f4988b
lenard123 <lenard.mangayayam@gmail.com>
2023-04-26 02:44:17 +0000

    WIP

--- resources/js/pages/Cashier/Cashier.jsx
+++ resources/js/pages/Cashier/Cashier.jsx
@@ -6,10 +6,12 @@ import useProductsQuery from "../../queries/useProductsQuery";
 import { ProductCard } from "./components/ProductCard";
 import { EmptyProducts } from "./components/EmptyProducts";
 import { CartItem } from "./components/CartItem";
+import { PaymentModal } from "./components/PaymentModal";
 
 
 
 export default function Cashier() {
+    const [open, setOpen] = useState(false)
     const [activeCategory, setActiveCategory] = useState(null);
     const { data: products, isLoading } = useProductsQuery(activeCategory);
     const [cart, setCart] = useState([]);
@@ -19,96 +21,109 @@ export default function Cashier() {
                 if (activeCategory === null) setActiveCategory(data[0]?.id);
             },
         });
-    
-    const total = cart.reduce((acm,item) => {
-        return acm + item.quantity * item.price
-    }, 0)
+
+    const total = cart.reduce((acm, item) => {
+        return acm + item.quantity * item.price;
+    }, 0);
+
+    const handlePayment = () => {
+        if (cart.length === 0) return;
+        setOpen(true)
+    }
 
     return (
-        <div className="flex h-full">
-            <div className="flex-grow p-8 overflow-y-auto thin-scrollbar">
-                <div className="flex justify-between items-center">
-                    <div>
-                        <h1 className="text-slate-800 text-2xl font-bold">
-                            Welcome, Garry
-                        </h1>
-                        <div className="text-sm text-slate-500">
-                            Discover whatever you need easily
+        <>
+            <PaymentModal open={open} setOpen={setOpen} total={total}/>
+            <div className="flex h-full">
+                <div className="flex-grow p-8 overflow-y-auto thin-scrollbar">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h1 className="text-slate-800 text-2xl font-bold">
+                                Welcome, Garry
+                            </h1>
+                            <div className="text-sm text-slate-500">
+                                Discover whatever you need easily
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div className="bg-white py-2 px-3 shadow-sm w-64 focus-within:w-80 transition-all font-light focus-within:shadow-md">
-                            <i className="far fa-search text-slate-500" />
-                            <input
-                                className="focus:outline-none ml-2"
-                                type="search"
-                                placeholder="Search product"
-                            />
-                        </div>
-                    </div>
-                </div>
-                {fetchingCategories ? (
-                    <Loading />
-                ) : (
-                    <>
-                        <div className="mt-6 flex gap-4">
-                            {categories.map((category) => (
-                                <CategoryButton
-                                    setCategory={setActiveCategory}
-                                    active={category.id === activeCategory}
-                                    key={category.id}
-                                    {...category}
+                        <div>
+                            <div className="bg-white py-2 px-3 shadow-sm w-64 focus-within:w-80 transition-all font-light focus-within:shadow-md">
+                                <i className="far fa-search text-slate-500" />
+                                <input
+                                    className="focus:outline-none ml-2"
+                                    type="search"
+                                    placeholder="Search product"
                                 />
-                            ))}
+                            </div>
                         </div>
-                        {isLoading ? (
-                            <Loading />
-                        ) : (
-                            <div className="mt-6 flex gap-4 flex-wrap">
-                                {products.length === 0 && <EmptyProducts />}
-                                {products.map((product) => (
-                                    <ProductCard
-                                        cart={cart}
-                                        setCart={setCart}
-                                        key={product.id}
-                                        {...product}
+                    </div>
+                    {fetchingCategories ? (
+                        <Loading />
+                    ) : (
+                        <>
+                            <div className="mt-6 flex gap-4">
+                                {categories.map((category) => (
+                                    <CategoryButton
+                                        setCategory={setActiveCategory}
+                                        active={category.id === activeCategory}
+                                        key={category.id}
+                                        {...category}
                                     />
                                 ))}
                             </div>
-                        )}
-                    </>
-                )}
-            </div>
-            <div className="bg-white w-80 flex-shrink-0 shadow-sm p-4 py-8 flex flex-col">
-                <div className="flex justify-between items-center">
-                    <h1 className="text-slate-800 text-2xl font-bold">
-                        Current Order
-                    </h1>
-                    <button className="bg-slate-100 p-1 px-3 shadow-sm">
-                        <i className="far fa-trash text-sm text-slate-500 block" />
-                    </button>
+                            {isLoading ? (
+                                <Loading />
+                            ) : (
+                                <div className="mt-6 flex gap-4 flex-wrap">
+                                    {products.length === 0 && <EmptyProducts />}
+                                    {products.map((product) => (
+                                        <ProductCard
+                                            cart={cart}
+                                            setCart={setCart}
+                                            key={product.id}
+                                            {...product}
+                                        />
+                                    ))}
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
-                <div className="mt-8 flex-grow">
-                    <div className="flex flex-col gap-6">
-                        {cart.map((item) => (
-                            <CartItem cart={cart} setCart={setCart} key={item.id} item={item} />
-                        ))}
+                <div className="bg-white w-80 flex-shrink-0 shadow-sm p-4 py-8 flex flex-col">
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-slate-800 text-2xl font-bold">
+                            Current Order
+                        </h1>
+                        <button className="bg-slate-100 p-1 px-3 shadow-sm">
+                            <i className="far fa-trash text-sm text-slate-500 block" />
+                        </button>
                     </div>
-                </div>
-                <div className="mt-4">
-                    <div className="bg-slate-100 p-4">
-                        <div className="flex justify-between text-sm">
-                            <div className="text-slate-500">Subtotal</div>
-                            <div className="text-slate-700 font-bold">
-                                ₱{total}
-                            </div>
+                    <div className="mt-8 flex-grow">
+                        <div className="flex flex-col gap-6">
+                            {cart.map((item) => (
+                                <CartItem
+                                    cart={cart}
+                                    setCart={setCart}
+                                    key={item.id}
+                                    item={item}
+                                />
+                            ))}
                         </div>
                     </div>
-                    <button className="w-full bg-primary text-white py-2 mt-4">
-                        Process Order
-                    </button>
+                    <div className="mt-4">
+                        <div className="bg-slate-100 p-4">
+                            <div className="flex justify-between text-sm">
+                                <div className="text-slate-500">Subtotal</div>
+                                <div className="text-slate-700 font-bold">
+                                    ₱{total}
+                                </div>
+                            </div>
+                        </div>
+                        <button onClick={handlePayment} className="w-full bg-primary text-white py-2 mt-4">
+                            Proceed to payment
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
---
+++ resources/js/pages/Cashier/components/PaymentModal.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+
+export const PaymentModal = ({ open, setOpen }) => {
+    const [amount, setAmount] = useState("");
+
+    const handleBackSpace = () => {
+        if (amount.length > 0)
+            setAmount((amount) => amount.substring(0, amount.length - 1));
+    };
+
+    const handleCancel = () => {
+      setAmount("")
+      setOpen(false)
+    }
+
+    if (!open) return null
+
+    return (
+        <div className="fixed inset-0 bg-black/50 z-10 flex items-center justify-center">
+            <div className="bg-white shadow w-96 p-4">
+                <div>Enter Payment:</div>
+                <div className="bg-slate-300 p-4 text-left text-xl">
+                    ₱{amount}
+                </div>
+
+                <div className="grid grid-cols-3 gap-2 mt-2">
+                    <button
+                        onClick={() => setAmount((amount) => amount + "7")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        7
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "8")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        8
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "9")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        9
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "4")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        4
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "5")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        5
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "6")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        6
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "1")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        1
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "2")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        2
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "3")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        3
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + ".")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        .
+                    </button>
+                    <button
+                        onClick={() => setAmount((amount) => amount + "0")}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        0
+                    </button>
+                    <button
+                        onClick={handleBackSpace}
+                        className="text-xl text-black bg-slate-100 py-4 shadow"
+                    >
+                        <i className="fal fa-arrow-left"></i>
+                    </button>
+                </div>
+                <button className="w-full bg-primary text-white py-2 mt-4">
+                    Process Order
+                </button>
+
+                <button onClick={handleCancel} className=" text-black bg-slate-100 py-2 shadow w-full mt-2">
+                    Cancel
+                </button>
+            </div>
+        </div>
+    );
+};
